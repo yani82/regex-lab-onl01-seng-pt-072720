cfd6e12b07ca05816ed89f931cff8c5af8423a9e
Yani Asari <yani.iman1@gmail.com>
2020-08-09 20:28:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.match(/^[A-Z].+/)
+if text.match(/^[A-Z].+[]$/)
 end
 
 def valid_phone_number?(phone)
