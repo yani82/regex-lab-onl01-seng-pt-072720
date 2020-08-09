9136ebbe724c85c5b50e82fa250e37bd47bff716
Yani Asari <yani.iman1@gmail.com>
2020-08-09 20:18:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ if word.match(/^[aeiouAEIOU]+\w/) then
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-if text.(/[^un][$ing])
+if text.scan(/[^un][$ing])
 end
 
 def words_five_letters_long(text)
