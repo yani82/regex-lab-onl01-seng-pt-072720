ccda3c8859dea3ea48f3a9c2709ad0bfd2a9e4c0
Yani Asari <yani.iman1@gmail.com>
2020-08-09 20:19:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,8 +7,7 @@ if word.match(/^[aeiouAEIOU]+\w/) then
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-if text.scan(/[^un][$ing]/) 
-  return "#{text}"
+text.scan(/[^un][$ing]/) 
 end
 
 def words_five_letters_long(text)
