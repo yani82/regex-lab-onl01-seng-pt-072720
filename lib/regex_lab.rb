f017f921f8c675e76575f183506845599178fe57
Yani Asari <yani.iman1@gmail.com>
2020-08-09 20:30:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.match(/^[A-Z].+[\.?!]$/) then 
+if text.match(/^[A-Z].+[\.!?]$/) then 
   return true
 else 
   return false
